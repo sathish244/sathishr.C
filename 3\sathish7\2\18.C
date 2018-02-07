ca6943c75e868e7f948bef476fe8c71bab335ffb
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-02-07 16:20:46 +0530

    3\sathish7\2\18.c

---
+++ 3\sathish7\2\18.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the value \n");
+scanf("%d",&a);
+printf(" Enter the value \n");
+scanf("%d",&b);
+c=pow(a,b);
+printf("%d \n",c);
+getch();
+return 0;
+}
