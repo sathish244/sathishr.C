56398d664b3a02920a3622e705330b15c60c1b93
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-02-12 15:53:06 +0530

    12\2\18sathish.C

---
+++ 12\2\18sathish1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,num;
+clrscr();
+printf("enter the a,b value");
+scanf("%d%d",&a,&b);
+{
+for(i=a;i<=b,i++)
+{
+if(i%2==1)
+printf("print the i value%d",i);
+}
+}
+getch();
+return 0;
+}
