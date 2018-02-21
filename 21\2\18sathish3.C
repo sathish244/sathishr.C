4fad2088caa1866ea31ce929426243a1ffe7d614
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-02-21 16:24:54 +0530

    21\2\18sathish3.C

---
+++ 21\2\18sathish3.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h> 
+int main() 
+{
+int i,n, arr[100];
+clrscr();
+printf("enter the 1 to 100 value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter the number:",i+1);
+scanf("%d",&arr[i]);
+}
+for(i=0;i<=n;i++)
+{
+if(arr[0]>arr[i])
+a[0]=a[i];
+}
+printf("minimum element%d",a[0]);
+getch();
+return 0;
+}
