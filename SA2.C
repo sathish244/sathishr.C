e440e17a895ca3209b5a051964cea852b92deeee
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-05 15:54:57 +0530

    SA2.C

---
+++ SA2.C
@@ -0,0 +1,17 @@
+include <stdio.h>
+void main() 
+{
+char str[50];
+int i,count=0;
+clrscr();
+printf("enter the sentence:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]==' ')
+{
+count=count+1;
+}
+}
+printf("\nno of white spaces=%d",count);
+}
