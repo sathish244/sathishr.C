adda628f6fd4a44e3b56816e862bb0894b6c8b11
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-07 16:02:33 +0530

    SA40.C

---
+++ SA40.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+int a=1,b=1,i,n,c;
+clrscr();
+printf("\n enter the range to print the Fibonacci numbers\n");
+scanf("%d",&n);
+printf("%d",a);
+printf("%d",b);
+for(i=1;i<=n-2;i++)
+{
+c=a+b;
+printf("%d",c);
+a=b;
+b=c;
+}
+return 0;
+}
