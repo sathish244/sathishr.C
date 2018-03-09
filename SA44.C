14bb1c3411f52aacb80850efff5d3667a6c13781
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-09 13:31:02 +0530

    SA44.C

---
+++ SA44.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main) 
+{
+int n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+if(n>=1 && n<=10)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
