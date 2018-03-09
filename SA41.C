921a0ae6d14e97b874e71235cbf10895d527fd2c
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-09 13:12:29 +0530

    SA41.C

---
+++ SA41.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+char s[50];
+int k,i;
+clrscr();
+printf("enter the string:");
+scanf("%s",s);
+printf("enter the number:");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n%s",s);
+}
+return 0;
+}
