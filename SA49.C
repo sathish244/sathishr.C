0e407f492730f353f2b481972e73fdc5fd5a8d84
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-12 15:59:59 +0530

    SA49.C

---
+++ SA49.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+int n1,n2,sum;
+clrscr();
+printf("\n enter the first number:");
+scanf("%d",&n1);
+printf("\n enter the second number:");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("%d",sum);
+return 0;
+}
