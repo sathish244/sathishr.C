f248043f1f3e40012589fa9f124ad8f3cc1b833f
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-14 16:03:41 +0530

    SA53.C

---
+++ SA53.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+int num,temp,sum=0;
+clrscr();
+printf("enter the number");
+scanf("%d",&num);
+temp=num;
+while(num!=0)
+{
+temp=num%10;
+sum=sum+temp;
+num=num/10;
+}
+printf("\n%d",sum);
+return 0;
+}
