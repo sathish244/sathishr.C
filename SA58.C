a18fe8ace60f58d5eef03b11b36cb396fce68d15
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-16 03:30:31 -0700

    SA58.C

---
+++ SA58.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+int x,y,temp;
+clrscr();
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&x,&y);
+printf("\n before swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+temp=x;
+x=y;
+y=temp;
+printf("\n after swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+return 0;
+}
