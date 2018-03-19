a72b02449b740cd125236a3715a1cdf5bcb71996
sathish244 <35688021+sathish244@users.noreply.github.com>
2018-03-19 16:15:09 +0530

    Update SA63.C

--- SA63.C
+++ SA63.C
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 int main() 
 {
 char str[50];
 int i,count=1;
+clrscr();
 printf("enter the sentence:");
 scanf("%[^\n]s",str);
 for(i=0;str[i]!='\0';i++)
@@ -14,4 +16,5 @@ count=count+1;
 }
 printf("\nno of words=%d",count);
 return 0;
+getch();
 }
